6d1495e060b84f453c20a2432d2a6ab01975cddb
sejalkulkarni <sejalkulkarni017@gmail.com>
2026-04-04 18:36:50 +0530

    count file added and app.js updated

--- helloworldapp (1)/helloworldapp/src/App.js
+++ helloworldapp (1)/helloworldapp/src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 import Container from './Layout/Container';
 import Dashboard from './Dashboard';
-
+import CounterExample from './Count';
 function App() {
   return (
     <div className="App">
      {/* <Container></Container> */}
-     <Dashboard></Dashboard>
+     {/* <Dashboard></Dashboard> */}
+     <CounterExample></CounterExample>
     </div>
   );
 }
--- helloworldapp (1)/helloworldapp/src/Count.js
+++ helloworldapp (1)/helloworldapp/src/Count.js
@@ -1,12 +1,20 @@
+import {useState , useEffect} from 'react';
+
 function CounterExample() {
 
 const [count, setCount]=useState(0);
 
-
+useEffect(()=> {
+    document.title=`you clicked ${count} times`;
+});
     return(
         <div>
-            <p>you clicked {count}</p>
+            <p>You clicked {count}</p>
             <button onClick={ ()=> setCount(count+1)}> click me </button>
+            <button onClick={ ()=> setCount(count-1)}>  Reset </button>
         </div>
+       
     )
 } 
+
+export default CounterExample;
